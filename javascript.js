a511a9e5097a7b058fa5ee9b1bbb92b41282df6e
Victor <rusuvictor30@gmail.com>
2019-10-23 15:24:05 -0400

    Javascript step

---
+++ javascript.js
@@ -0,0 +1,36 @@
+// User Class
+class User {
+    constructor(fname,lname,location) {
+        this.fname = fname;
+        this.lname = lname;
+        this.location = location;
+    }
+}
+
+// UI Class
+class UI {
+    static displayUsers(){
+        const StoredUsers = [
+            {
+                fname: 'Ion',
+                lname: 'Rusu',
+                location: 'Ungheni'
+            },
+            {
+                fname: 'Vasile',
+                lname: 'Rotaru',
+                location: 'Petresti'
+            }
+        ];
+
+        const users = StoredUsers;
+    }
+}
+
+// Store Class
+
+// Display User
+
+// Add User
+
+// Remove Users
